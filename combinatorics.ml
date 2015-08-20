95310265ac9db8ef1f1f001850712d82ef9097f6
Edgar Liberis <edgaras.liberis@gmail.com>
2015-08-21 00:37:54 +0100

    pairs

--- combinatorics.ml
+++ combinatorics.ml
@@ -8,5 +8,20 @@ module Combinatorics = struct
     match l with
     | [] -> [[]]
     | h :: t -> (* Could do reverse flattening for efficiency *)
-      List.flatten (List.map (insert_everywhere h) (permutations t))
+      List.flatten (List.map (insert_everywhere h) (permutations t));;
+
+  let pairs (elems : 'a list) : ('a * 'a) list list =
+    let es = Array.of_list elems in
+    let n = Array.length es in
+    let make_pair day a_idx = (es.(a_idx), es.((a_idx + day) mod n)) in
+    let iterate_pairs func times = 
+      let pair_list = ref [] in
+      for i = 0 to times - 1 do 
+        pair_list := func i :: !pair_list done; 
+      !pair_list in
+    let result = ref [] in
+    for day = 1 to n-1 do 
+      result := (iterate_pairs (make_pair day) (n / 2)) :: !result
+    done;
+    !result
 end
