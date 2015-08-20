55907cdc2bf5938a0b558681f208e1cf3d68eacb
Edgar Liberis <edgaras.liberis@gmail.com>
2015-08-20 11:43:45 +0100

    finished lru

--- functional_primitives.ml
+++ functional_primitives.ml
@@ -11,50 +11,58 @@ module Memoization = struct
     handle
 end
 
-let func a = a + 1;;
+let func a = print_string "func was called!"; a + 1;;
 
-type 'a llnodeelement = None | Some of ('a * ('a llnode))
+type 'a llnodeelement = Nil | Elem of ('a * ('a llnode))
  and 'a llnode = 'a llnodeelement ref;;
 
 class ['a] linked_list limit = 
   object (self)
-    val list = ref None
+    val mutable list = ref Nil
+    val size = ref 0
 
-    method remove_last = 
+    method get_first_node () = !list
+
+    method remove_last () = 
       let rec loop (l : 'a llnode)  = 
         match !l with
-        | None -> failwith "can't remove element from an empty list"
-        | Some (_, nref) -> begin
+        | Nil -> failwith "can't remove element from an empty list"
+        | Elem (_, nref) -> begin
           match !nref with
-          | None -> l := None
-          | Some (_, _) -> loop nref
+          | Nil -> l := Nil
+          | Elem (_, _) -> loop nref
         end in
       loop list
 
-    method add elem =
-      list := Some (elem, list);
+    method add (elem : 'a) =
+      list <- ref (Elem (elem, list));
       size := !size + 1
 
-    method conditional_add elem =
-      if !size > limit then self#remove_last;
+    method conditional_add (elem : 'a) =
+      if !size > limit then self#remove_last ();
       self#add elem
 
-    method take pred =
-      let rec loop (l : 'a llnode) =
+    method remove (l : 'a llnode) =
+      match !l with
+      | Nil -> failwith "node is an empty list"
+      | Elem (_, child) -> (* copy l's child into l *)
+        l := !child; 
+
+    method take (pred : 'a -> bool) : 'a option =
+      let rec loop (l : 'a llnode) : 'a option =
         match !l with 
-        | None -> None
-        | Some (v, next) -> if (pred v) then  else loop next in
+        | Nil -> None
+        | Elem (v, next) -> if (pred v) then (self#remove l; Some v) else loop next in
       loop list
   end;;
 
 (* Memoisation wrapper utilising LinkedList for LRU caching *)
 module MemoList = struct 
-  let memoize f = 
-    let memos = new linked_list 20 in
-    let handle arg = 
+  let memoize (f : 'a -> 'b) = 
+    let memos = new linked_list 5 in
+    let handle (arg : 'a) : 'b = 
       match memos#take (fun (k, v) -> k = arg) with
-      | None -> let v = f arg in memos#conditional_add v; v  
-      | Some (k, v) -> v
+      | None -> let v = f arg in (memos#conditional_add (arg, v); v)
+      | Some (_, v) -> (memos#add (arg, v); v) in
     handle
-
 end
