56d59834433a317783487624f52c5877cbb8e0b0
Edgar Liberis <edgaras.liberis@gmail.com>
2015-08-21 15:21:52 +0100

    picking best list

--- misc.ml
+++ misc.ml
@@ -1,4 +1,7 @@
 let rec group_by (pred : 'a -> 'a -> bool) (l : 'a list) : 'a list list =
+  (* Splits the list into two lists, 
+     left with elements that satisfy the predicate,
+     right with ones that don't *)
   let rec split_by (pred : 'a -> bool) (elems : 'a list) =
     match elems with
     | [] -> ([], [])
@@ -7,7 +10,7 @@ let rec group_by (pred : 'a -> 'a -> bool) (l : 'a list) : 'a list list =
       if pred h then (h :: matching, not_matching) else (matching, h :: not_matching) in
   match l with
   | [] -> []
-  | h :: t -> 
+  | h :: t -> (* Get matching elements together and group the non-matching rest *)
     let matching, not_matching = split_by (pred h) t in
     (h :: matching) :: (group_by pred not_matching);;
 
@@ -44,3 +47,22 @@ let rot_left arr n =
     reverse_arr arr 0 (n-1);
     reverse_arr arr n (len-1);
     reverse_arr arr 0 (len-1);;
+
+(* Get's the best element in the list according to some metric
+   eval:    provide an evaluation function, that would assign score to the elements
+            (higher is better)
+   l:       list of elements to choose from, has to contain at least one item
+   returns: element with the highest score, along with the score value 
+ *)
+let get_best (eval: 'b -> 'a) (l : 'b list) =
+  let update (curr : ('a * 'b)) (next : 'b) : ('a * 'b) =
+    let curr_score, curr_elem = curr in
+    let next_score = eval next in
+    if compare curr_score next_score < 0 then (next_score, next) else curr in
+  match l with
+  | [] -> failwith "no elements provided"
+  | b :: rest -> List.fold_left update (eval b, b) rest;;
+
+let sum_list = List.fold_left ( + ) 0;;
+let len_list = List.length;;
+
