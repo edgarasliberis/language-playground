473611d9aca6a1d63e99b3b30efbc35d678e4649
Edgar Liberis <edgaras.liberis@gmail.com>
2015-08-21 16:41:52 +0100

    data update

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ Functional primitives (OCaml) [see functional_primitives.ml]:
 * Memoisation wrapper. Optionally cache control.
 * Map, foldl, foldr, filter + tail recursive where appropriate; explain, write types
 
-Data structures (that functional data structures book will be useful here):
+Data structures (that functional data structures book will be useful here) [see data.ml]:
 * Priority queue using a stack
-* Linked list. Given a collection of lists, return one with minimum length / minimum sum of elements.
+* Linked list. 
 * Functional (+ Array) heap, making invariants hold, all operations (insert, delete, delete subtree recursively)
 * Binary (+search) tree and all operations (insert, delete, traverse, from-to list, length, depth, k-th child) 
 
@@ -26,3 +26,4 @@ Misc [see misc.ml]:
 * Given a set of strings, return a set of [lists of anagrams; pairs of palindromes; lists of strings with same acronyms]
 * Rotate array n times in constant space and linear time
 * Find nth element in merged sorted arrays
+* Given a collection of lists, return one with minimum length / minimum sum of elements.
--- data.ml
+++ data.ml
@@ -1,3 +1,5 @@
+(* All data structures presented here are mutable *)
+
 module SillyPriorityQueue = struct
   type 'a pqueue = {mutable queue : 'a list; mutable tqueue : 'a list };;
   let create () = { queue = []; tqueue = [] };;
@@ -21,4 +23,34 @@ module SillyPriorityQueue = struct
   let push q item =
     q.tqueue <- item :: q.tqueue;;
 
+end
+
+module LinkedList = struct 
+  type 'a llnode = { mutable value : 'a; mutable next : 'a llnode option }
+  type 'a llist = 'a llnode option ref
+
+  let create () : 'a llist = ref None;;
+
+  let push (l : 'a llist) (elem : 'a) : unit =
+    l := Some {value = elem; next = !l};;
+
+  let top (l : 'a llist) : 'a =
+    match !l with
+    | None -> failwith "supplied list is empty"
+    | Some {value = v; next = _} -> v;;
+
+  let pop (l : 'a llist) : unit =
+    match !l with
+    | None -> failwith "supplied list is empty"
+    | Some {value = _; next = next} -> l := next;;
+
+  let find (l : 'a llist) (elem : 'a) : 'a llist =
+    let rec look (l : 'a llnode option) =
+      match l with
+      | None -> failwith "given element was not found"
+      | Some {value = v; next = _} when v = elem -> ref l
+      | Some {value = _; next = n} -> look n in
+    look !l;;
+
+  (* TODO: delete, fold_left, map *)
 end
